a503644e6e3f4078362470ea841c0ec4ff3d5b1e
huaerjie <286252682@qq.com>
2016-12-16 14:15:20 +0800

    1.目录结构的优化

---
+++ ZPHP/Coroutine/Mysql/MySqlCoroutine.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoye
+ * Date: 16/9/14
+ * Time: 下午3:47
+ */
+
+
+namespace ZPHP\Coroutine\Mysql;
+
+use ZPHP\Core\Log;
+use ZPHP\Coroutine\Base\CoroutineResult;
+use ZPHP\Coroutine\Base\ICoroutineBase;
+
+class MySqlCoroutine implements ICoroutineBase{
+    /**
+     * @var MysqlAsynPool
+     */
+    public $_mysqlAsynPool;
+    public $bind_id;
+    public $sql;
+    public $result;
+
+    public function __construct($mysqlAsynPool)
+    {
+        $this->result = CoroutineResult::getInstance();
+        $this->_mysqlAsynPool = $mysqlAsynPool;
+    }
+
+    public function query($sql){
+        $this->sql = $sql;
+        $data = yield $this;
+        return $data;
+    }
+
+
+    /**
+     * @param $callback
+     * @throws \Exception
+     */
+    public function send(callable $callback)
+    {
+        $this->_mysqlAsynPool->query($callback, $this->sql);
+    }
+
+    public function getResult()
+    {
+        return $this->result;
+    }
+}
